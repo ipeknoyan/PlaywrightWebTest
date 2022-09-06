5f122fd68c790d8e4a03d08effba20ba850ebe1b
ipeknoyan <noyanipek@gmail.com>
2022-09-06 18:56:48 +0300

    comments are added

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,11 +1,13 @@
 import { expect, Locator, Page } from '@playwright/test';
 export class TesonetHomePage {
+    
 readonly url ="https://www.tesonet.com";
 readonly page: Page;
 readonly tesonetLogo: Locator;
 readonly careerMenu: Locator;
 readonly careerLever: Locator
 
+// create a locators
 constructor(page: Page) {
     this.page = page;
     this.tesonetLogo = page.locator('//a[@class="brand"]/img[@alt="tesonet-logo"]');
@@ -13,18 +15,27 @@ constructor(page: Page) {
     this.careerLever = page.locator('//*[@class="career-lever career-lever-v3"]')
 }
 
+// create keywords for test cases
 async goto(){
+    //Browse the page
     await this.page.goto(this.url);
+    //Make sure page has the correct title
+    await expect(this.page).toHaveTitle('We are Tesonet. We build the digital future.')
     }
 
 async clickOnCareer(){
+    // Wait for manu is visible
     await this.careerMenu.waitFor({state:"visible"});
+    // Click on career menu
     await this.careerMenu.click();
-    await this.careerLever.waitFor();
-    
+     // Make sure career page is opened
+    await this.careerLever.waitFor();  
     }
+
 async getCategoriesLength(){
+    //Get available categories in carees page
     const categories = await this.page.$$('//div[@class="career-lever-v3__job-category-box p-7 pb-lg-0 px-lg-8 pt-lg-8"]');
+    //Count and log the number of available categories in carees page
     console.log(categories.length)
     }
 }
--- tests/home.tests.ts
+++ tests/home.tests.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
 import { TesonetHomePage } from '../pages/home.page';
 
+//This case counts available ctegories
 test('Tesonet counting career categories ', async ({ page }) => {
     const homepage = new TesonetHomePage(page);
+    //Browse the page
     await homepage.goto();
+    //Click the Career Menu
     await homepage.clickOnCareer();
+    //Count the number of categories
     await homepage.getCategoriesLength();
     });
 
